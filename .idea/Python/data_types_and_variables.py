6b307bcd53b3895798cbd627d60475ec34172b1d
Fairuznewaz <fairuznewaz@gmail.com>
2024-09-10 14:28:55 -0400

    python practice

---
+++ .idea/Python/data_types_and_variables.py
@@ -0,0 +1,17 @@
+#basic data types
+age = 25         # Integer
+pi = 3.14        # Float
+name = "fairuz"   # String
+is_active = True # Boolean
+
+#list
+fruits = ["apple", "banana", "cherry"]
+fruits.append("mango")
+print(fruits[0])  # Access the first element
+
+#tuple
+coordinates = (10, 20)
+
+#dictionary
+student = {"name": "fairuz", "age": 25}
+print(student["name"])
