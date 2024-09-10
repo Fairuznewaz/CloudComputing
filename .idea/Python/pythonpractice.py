77014fea80ad6296cc4ef84b3802c5981a276437
Fairuznewaz <fairuznewaz@gmail.com>
2024-09-10 14:21:23 -0400

    python practice

---
+++ .idea/Python/pythonpractice.py
@@ -0,0 +1,10 @@
+#practice to add twol numbers
+
+num1 = 1.7
+num2 = 5.8
+
+#add two numbers
+sum = num1 + num2
+
+#display the result
+print('The sum of {0} and {1} is {2}'. format(num1, num2, sum))
