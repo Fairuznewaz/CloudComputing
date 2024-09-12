392a93444358cbe8e65188f20f8155b4f5a6db42
Fairuznewaz <fairuznewaz@gmail.com>
2024-09-12 04:47:50 -0400

    python lab number 1

--- .idea/Python_lab/1.py
+++ .idea/Python_lab/1.py
@@ -50,6 +50,7 @@ current_year = 2024
 birth_year = current_year - age
 print("Your birth yhear is:", birth_year)
 
+
 #Problem-7 : data types identification
 #int for integers (whole numbers).
 #'float' for floating-point numbers (decimals).
@@ -78,4 +79,11 @@ a, b = b, a
 
 print("After swapping: a =", a, "b =", b)
 
+#Problem-9 : Simple Interest Calculator
 
+principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the rate of interest: "))
+time = float(input("Enter the time (in years): "))
+
+simple_interest = (principal * rate * time) / 100
+print("The simple interest is:", simple_interest)
